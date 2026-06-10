a3a32d6eb8d754c33c702f95177083e86005d56d
bharadwajalyadalla <lyadallabharadwaja965@gmail.com>
2026-06-10 23:35:20 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,9 +1,8 @@
 import yfinance as yf
-import pandas as pd
+from ta.trend import EMAIndicator, MACD
+from ta.momentum import RSIIndicator
 
 symbol = "INFY.NS"
-
-print(f"Downloading data for {symbol}...")
 
 daily = yf.download(
     symbol,
@@ -13,28 +12,41 @@ daily = yf.download(
     progress=False
 )
 
-print("\n===== DATAFRAME INFO =====")
-print("Type of daily:", type(daily))
+close_daily = daily["Close"].iloc[:, 0]
 
-print("\n===== COLUMNS =====")
-print(daily.columns)
+ema100 = EMAIndicator(close_daily, window=100).ema_indicator()
+ema200 = EMAIndicator(close_daily, window=200).ema_indicator()
+rsi = RSIIndicator(close_daily, window=14).rsi()
 
-print("\n===== CLOSE COLUMN TYPE =====")
-print(type(daily["Close"]))
+print("DAILY")
 
-print("\n===== CLOSE COLUMN SHAPE =====")
-print(daily["Close"].shape)
+print("Close:", round(close_daily.iloc[-1], 2))
+print("EMA100:", round(ema100.iloc[-1], 2))
+print("EMA200:", round(ema200.iloc[-1], 2))
+print("RSI:", round(rsi.iloc[-1], 2))
 
-print("\n===== FIRST 5 ROWS =====")
-print(daily.head())
+intraday = yf.download(
+    symbol,
+    period="60d",
+    interval="30m",
+    auto_adjust=True,
+    progress=False
+)
 
-print("\n===== LAST 5 ROWS =====")
-print(daily.tail())
+close_30m = intraday["Close"].iloc[:, 0]
 
-print("\n===== LAST CLOSE =====")
+ema100_30 = EMAIndicator(close_30m, window=100).ema_indicator()
+ema200_30 = EMAIndicator(close_30m, window=200).ema_indicator()
 
-try:
-    close = daily["Close"].squeeze().iloc[-1]
-    print(close)
-except Exception as e:
-    print("ERROR:", e)
+macd = MACD(close_30m)
+
+rsi30 = RSIIndicator(close_30m, window=14).rsi()
+
+print("\n30 MIN")
+
+print("Close:", round(close_30m.iloc[-1], 2))
+print("EMA100:", round(ema100_30.iloc[-1], 2))
+print("EMA200:", round(ema200_30.iloc[-1], 2))
+print("RSI:", round(rsi30.iloc[-1], 2))
+print("MACD:", round(macd.macd().iloc[-1], 2))
+print("Signal:", round(macd.macd_signal().iloc[-1], 2))
